aca0ce7039275a717c7f5c4184af6891e3be3308
JJ Crossman <jj.crossman@gmail.com>
2016-09-23 15:25:35 -0500

    working on project

---
+++ index.js
@@ -0,0 +1,20 @@
+//REQUIRES
+const express = require( "express" );
+const { json } = require( "body-parser" );
+const session = require( "express-session" );
+const sessionConfig = require( "./config/sessionConfig.js" );
+const cors = require( "cors" );
+const corsOptions = require( "./config/corsOptions.js" )
+const app = express();
+const port = 8899;
+
+//MIDDLEWARE
+app.use( cors( corsOptions ) );
+app.use( json() );
+app.use( session( sessionConfig ) );
+app.use( express.static( `${ __dirname }/public` ) );
+
+//ROUTES
+
+//LISTEN
+app.listen( port, () => console.log( "User Profiles is listening on port: " + port ) );
