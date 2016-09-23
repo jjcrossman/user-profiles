152ff83dbc98e9621555f2c4b1b0934c966ff7b0
JJ Crossman <jj.crossman@gmail.com>
2016-09-23 18:15:56 -0500

    finished user-profiles, did not do black diamond

--- public/mainCtrl.js
+++ public/mainCtrl.js
@@ -2,8 +2,8 @@ angular.module('userProfiles')
 .controller('mainCtrl', function( $scope, friendService, $location ) {
 
 	$scope.login = function( user ) {
-		friendService.login(user).then(function( response ) {
-			if (response.data.userFound) {
+		friendService.login( user ).then(function( response ) {
+			if ( response.data.userFound ) {
 				$location.path('/profile');
 			} else {
 				alert('user not found');
